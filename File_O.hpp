d43ae556abfc85ed22b5b302dbe6aabc36c1ccfa
Yadunund <yadunund@openrobotics.org>
2020-02-26 19:00:31 +0800

    Indent check

--- File_O.hpp
+++ File_O.hpp
@@ -1,2 +1,31 @@
-while (a == b)
-  dothis;
+Planner::Configuration::Configuration(
+    Graph graph,
+    VehicleTraits traits,
+    Interpolate::Options interpolation)
+: _pimpl(rmf_utils::make_impl<Implementation>(
+      Implementation{
+        std::move(graph),
+        std::move(traits),
+        std::move(interpolation)
+      }))
+{
+  // Do nothing
+}
+
+relevant_changes.emplace_back(
+    Database::Change::Implementation::make_insert_ref(
+      &entry->trajectory, entry->version));
+
+
+result._pimpl = rmf_utils::make_impl<Implementation>(
+    Implementation{make_deep(std::move(trajectory)),
+      std::forward<Args>(args)...});
+
+result._pimpl = rmf_utils::make_impl<FinalShape::Implementation>(
+    FinalShape::Implementation{std::move(shape),
+    std::move(collisions),
+    characteristic_length});
+
+span._pimpl->maps = std::unordered_set<std::string>{
+    std::make_move_iterator(maps.begin()),
+    std::make_move_iterator(maps.end())};
