070ed00c3f53dc959a6633cf1a4099d412781a9d
Yadunund <yadunund@openrobotics.org>
2020-03-27 17:26:26 +0800

    Indent continued shit and do not align with previous <<

--- File.hpp
+++ File.hpp
@@ -1,245 +1,234 @@
-class Shape::Internal
+#ifndef SRC__BUILDING_SIM_COMMON__UTILS_HPP
+#define SRC__BUILDING_SIM_COMMON__UTILS_HPP
+
+#include <cmath>
+#include <iostream>
+
+namespace building_sim_common {
+
+// TODO(MXG): Refactor the use of this function to replace it with
+// compute_desired_rate_of_change()
+double compute_ds(
+  double s_target,
+  double v_actual,
+  double v_max,
+  double accel_nom,
+  double accel_max,
+  double dt);
+
+struct MotionParams
 {
-public:
-
-  virtual CollisionGeometries make_fcl() const = 0;
-
+  double v_max = 0.2;
+  double a_max = 0.1;
+  double a_nom = 0.08;
+  double dx_min = 0.01;
+  double f_max = 10000000.0;
 };
 
-Planner::Configuration::Configuration(
-  Graph graph,
-  VehicleTraits traits,
-  Interpolate::Options interpolation)
-: _pimpl(rmf_utils::make_impl<Implementation>(
-    Implementation{
-      std::move(graph),
-      std::move(traits),
-      std::move(interpolation)
-      }))
+//==============================================================================
+double compute_ds(
+  double s_target,
+  double v_actual,
+  const double v_max,
+  const double accel_nom,
+  const double accel_max,
+  const double dt)
 {
-  // Do nothing
-}
-
-relevant_changes.emplace_back(
-  make_insert_ref{
-    &entry->trajectory, entry->version});
-
-relevant_changes.emplace_back(
-  Database::Change::Implementation::make_insert_ref(
-    &entry->trajectory, entry->version));
-
-elements.emplace_back(
-  Viewer::View::Element{
-    entry->version, entry->trajectory});
-
-result._pimpl = rmf_utils::make_impl<Implementation>(
-  Implementation{make_deep(std::move(trajectory)),
-    std::forward<Args>(args)...});
-
-result._pimpl = rmf_utils::make_impl<FinalShape::Implementation>(
-  FinalShape::Implementation{std::move(shape),
-    std::move(collisions),
-    characteristic_length});
-
-span._pimpl->maps = std::unordered_set<std::string>{
-  std::make_move_iterator(maps.begin()),
-  std::make_move_iterator(maps.end())};
-
-for (Timeline::iterator it = timeline.begin(); it != end_it; ++it)
-{
-  Bucket& bucket = it->second;
-  const Bucket::iterator removed =
-    std::remove_if(bucket.begin(), bucket.end(),
-    [&](const internal::ConstEntryPtr& entry) -> bool
+  double sign = 1.0;
+  if (s_target < 0.0)
   {
-    return *entry->trajectory.finish_time() < time;
-  });
-
-  for (Bucket::iterator bit = removed; bit != bucket.end(); ++bit)
-    culled.insert((*bit)->version);
-
-  bucket.erase(removed, bucket.end());
-}
-
-DerivedExecutor& execute(DerivedExecutor& executor) const
-{
-  return static_cast<DerivedExecutor&>(
-    execute(static_cast<Executor&>(executor)));
-}
-
-if (destination->data.finish_time == new_time)
-{
-  // The new time conflicts with an existing time, so we will throw an
-  // exception.
-  throw std::invalid_argument(
-    "[Trajectory::Segment::set_finish_time] Attempted to set time to "
-    + std::to_string(new_time.time_since_epoch().count())
-    + "ns, but a waypoint already exists at that timestamp.");
-}
-
-Trajectory::InsertionResult Trajectory::insert(
-  Time finish_time,
-  ConstProfilePtr profile,
-  Eigen::Vector3d position,
-  Eigen::Vector3d velocity)
-{
-  return _pimpl->insert(
-    internal::SegmentElement::Data{
-      std::move(finish_time),
-      std::move(profile),
-      std::move(position),
-      std::move(velocity)});
-}
-
-bool VehicleTraits::valid() const
-{
-  const bool steering_valid = [&]() -> bool
-  {
-    if (_pimpl->_steering_mode == Steering::Differential)
-      return get_differential()->valid();
-
-    return true;
-  } ();
-
-  return linear().valid() && rotational().valid() && steering_valid;
-}
-
-//
-
-bool A::a() const
-{
-  const bool c =
-    [&]() -> bool
-  {
-    if (_pimpl->_steering_mode == Steering::Differential)
-      return get_differential()->valid();
-
-    return true;
-  } ();
-
-  return linear().valid() && rotational().valid() && steering_valid;
-}
-
-queue.emplace(std::make_shared<Node>(
-  Node{
-    args.waypoint,
-    estimate_remaining_cost(location),
-    0.0,
-    location,
-    nullptr
-  }));
-
-
-void func()
-{
-  return Result{
-    std::move(trajectories),
-    std::move(waypoints),
-    starts[start_index],
-    std::move(goal),
-    std::move(options)
-  };
-}
-
-namespace p {
-namespace q {
-
-const auto remove_it = std::remove_if(
-  _pimpl->itinerary.begin(),
-  _pimpl->itinerary.end(),
-  [&](const Writer::Item& item)
-    {
-      return input_routes.count(item.id) > 0;
-});
-
-} // namespace q
-} // namespace p
-
-foo.emplace(std::make_shared<Bar>(
-  Bar{
-    a,
-    b,
-    b,
-  }));
-
-
-foo.emplace_back(
-  Bar{
-    a,
-    b,
-    c
-  });
-
-Foo::Foo(
-  A a,
-  B b)
-: _bar(std::make_shared<Bar>(
-    Bar{
-      a,
-      b,
-      }))
-{
-
-}
-
-if (min_size < 2)
-{
-  throw invalid_trajectory_error::Implementation
-        ::make_segment_num_error(min_size);
-}
-
-const Trajectory::const_iterator begin_it =
-  trajectory_start_time < start_time ?
-  trajectory.find(start_time) : ++trajectory.begin();
-
-const Eigen::Vector2d p = n->waypoint ?
-  graph.waypoints[*n->waypoint].get_location() :
-  n->trajectory_from_parent.back().get_finish_position()
-  .template block<2, 1>(0, 0);
-
-rmf_utils::optional<Plan> Plan::replan(const Start& new_start) const
-{
-  return Plan::Implementation::generate(
-    _pimpl->cache_mgr, DifferentialDriveCache(
-    agv::Planner::Configuration config)
-      : _config(std::move(config)),
-      _graph(agv::Graph::Implementation::get(_config.graph())),
-      _traits(_config.vehicle_traits()),
-      _profile(_traits.get_profile()),
-      _interpolate(agv::Interpolate::Options::Implementation::get(
-        _config.interpolation()))
-  {
-      // Do nothing
+    // Limits get confusing when we need to go backwards, so we'll flip signs
+    // here so that we pretend the target is forwards
+    s_target *= -1.0;
+    v_actual *= -1.0;
+    sign = -1.0;
   }
 
-    {new_start},
-    _pimpl->result.goal,
-    _pimpl->result.options);
+  // We should try not to shoot past the targstd::vector<event::ConnectionPtr> connections;et
+  double next_s = s_target / dt;
+
+  // Test velocity limit
+  next_s = std::min(next_s, v_max);
+
+  // Test acceleration limit
+  next_s = std::min(next_s, accel_nom * dt + v_actual);
+
+  if (v_actual > 0.0 && s_target > 0.0)
+  {
+    // This is what our deceleration should be if we want to begin a constant
+    // deceleration from now until we reach the goal
+    double deceleration = pow(v_actual, 2) / s_target;
+    deceleration = std::min(deceleration, accel_max);
+
+    if (accel_nom <= deceleration)
+    {
+      // If the smallest constant deceleration for reaching the goal is
+      // greater than
+      next_s = -deceleration * dt + v_actual;
+    }
+  }
+
+  // Flip the sign the to correct direction before returning the value
+  return sign * next_s;
 }
 
-rmf_utils::optional<Plan> Planner::plan(const Start& start, Goal goal) const
+//==============================================================================
+double compute_desired_rate_of_change(
+  double _s_target,
+  double _v_actual,
+  const MotionParams& _motion_params,
+  const double _dt)
 {
-  return Plan::Implementation::generate(
-        _pimpl->cache_mgr,
-    {start},
-        std::move(goal),
-        _pimpl->default_options);
+  double sign = 1.0;
+  if (_s_target < 0.0)
+  {
+    // Limits get confusing when we need to go backwards, so we'll flip signs
+    // here so that we pretend the target is forwards
+    _s_target *= -1.0;
+    _v_actual *= -1.0;
+    sign = -1.0;
+  }
+
+  // We should try not to shoot past the target
+  double v_next = _s_target / _dt;
+
+  // Test velocity limit
+  v_next = std::min(v_next, _motion_params.v_max);
+
+  // Test acceleration limit
+  v_next = std::min(v_next, _motion_params.a_nom * _dt + _v_actual);
+
+  if (_v_actual > 0.0 && _s_target > 0.0)
+  {
+    // This is what our deceleration should be if we want to begin a constant
+    // deceleration from now until we reach the goal
+    double deceleration = pow(_v_actual, 2) / (2.0 * _s_target);
+    deceleration = std::min(deceleration, _motion_params.a_max);
+
+    if (_motion_params.a_nom <= deceleration)
+    {
+      // If the smallest constant deceleration for reaching the goal is
+      // greater than the nominal acceleration, then we should begin
+      // decelerating right away so that we can smoothly reach the goal while
+      // decelerating as close to the nominal acceleration as possible.
+      v_next = -deceleration * _dt + _v_actual;
+    }
+  }
+
+  // Flip the sign the to correct direction before returning the value
+  return sign * v_next;
 }
 
-foo.emplace_back(Bar{
-    a,
-    b,
-    c
-  });
-
-DifferentialDriveCache(agv::Planner::Configuration config)
-  : _config(std::move(config)),
-  _graph(agv::Graph::Implementation::get(_config.graph())),
-  _traits(_config.vehicle_traits()),
-  _profile(_traits.get_profile()),
-  _interpolate(agv::Interpolate::Options::Implementation::get(
-      _config.interpolation()))
+//==============================================================================
+template<typename SdfPtrT, typename SdfElementPtrT>
+bool get_element_required(
+  SdfPtrT& _sdf,
+  const std::string& _element_name,
+  SdfElementPtrT& _element)
 {
-  // Do nothing
+  if (!_sdf->HasElement(_element_name))
+  {
+    std::cerr << "Element [" << _element_name << "] not found" << std::endl;
+    return false;
+  }
+  _element = _sdf->GetElement(_element_name);
+  return true;
 }
+
+/*
+double compute_desired_rate_of_change(
+    double _s_target,
+    double _v_actual,
+    const MotionParams& _motion_params,
+    const double _dt);
+
+bool get_element_required(
+    const sdf::ElementPtr& _sdf,
+    const std::string& _element_name,
+    sdf::ElementPtr& _element);
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename T, typename SdfPtrT>
+bool get_sdf_attribute_required(SdfPtrT& sdf, const std::string& attribute_name,
+  T& value)
+{
+  if (sdf->HasAttribute(attribute_name))
+  {
+    if (sdf->GetAttribute(attribute_name)->Get(value))
+    {
+      std::cout << "Using specified attribute value [" << value
+        << "] for property [" << attribute_name << "]"
+        << std::endl;
+      return true;
+    }
+    else
+    {
+      std::cerr << "Failed to parse sdf attribute for [" << attribute_name
+        << "]" << std::endl;
+    }
+  }
+  else
+  {
+    std::cerr << "Attribute [" << attribute_name << "] not found" << std::endl;
+  }
+
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename T, typename SdfPtrT>
+bool get_sdf_param_required(SdfPtrT& sdf, const std::string& parameter_name,
+  T& value)
+{
+  if (sdf->HasElement(parameter_name))
+  {
+    if (sdf->GetElement(parameter_name)->GetValue()->Get(value))
+    {
+      std::cout << "Using specified value [" << value << "] for property ["
+        << parameter_name << "]" << std::endl;
+      return true;
+    }
+    else
+    {
+      std::cerr << "Failed to parse sdf value for [" << parameter_name << "]"
+        <<std::endl;
+    }
+  }
+  else
+  {
+    std::cerr << "Property [" << parameter_name << "] not found" << std::endl;
+  }
+
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename T, typename SdfPtrT>
+void get_sdf_param_if_available(SdfPtrT& sdf, const std::string& parameter_name,
+  T& value)
+{
+  if (sdf->HasElement(parameter_name))
+  {
+    if (sdf->GetElement(parameter_name)->GetValue()->Get(value))
+    {
+      std::cout << "Using specified value [" << value << "] for property ["
+        << parameter_name << "]" << std::endl;
+    }
+    else
+    {
+      std::cerr << "Failed to parse sdf value for [" << parameter_name
+        << "]" << std::endl;
+    }
+  }
+  else
+  {
+    std::cout << "Using default value [" << value << "] for property ["
+      << parameter_name << "]" << std::endl;
+  }
+}
+
+} // namespace building_sim_common
+
+#endif // SRC__BUILDING_SIM_COMMON__UTILS_HPP
 
